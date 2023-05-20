f02eccc3b09a1cdd852b15ee29a47dad727ee24f
musagenius345 <musabadru@gmail.com>
2023-05-20 13:44:54 +0300

    intial

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import preprocess from 'svelte-preprocess';
+
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
